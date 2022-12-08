a79c415364c14a01e5e5c4df25207c69a076a788
JackLeserman <39784006+JackLeserman@users.noreply.github.com>
2022-12-08 09:49:43 -0500

    Working Sound

--- sound/src/play.py
+++ sound/src/play.py
@@ -3,12 +3,10 @@ import rospy
 from std_msgs.msg import Int32
 
 def db_playsound(param: Int32):
-    rospy.loginfo("cum")
     os.system(f"play -n synth sin {param.data} trim 0 00:01")
 
 play_sub = rospy.Subscriber('/play_sound', Int32, db_playsound)
 
 if __name__ == "__main__":
-    rospy.loginfo("Piss")
     rospy.init_node("sound_node")
     rospy.spin()

dfe4f34793bdb31ea317409bdbedf19f972fcb99
Greg Lewin <gcl8a@icloud.com>
2023-07-31 20:48:02 -0400

    trying out a new state machine

---
+++ wire-bot.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from enum import Enum
+
+import rospy
+
+from std_msgs.msg import UInt16
+#from sensor_msgs.msg import BatteryState
+
+class states(Enum):
+    ROBOT_IDLE = 0, 
+
+    ROBOT_PATROL_FWD = 1
+    ROBOT_APPROACH_FWD = 2
+    ROBOT_DETERRENT_FWD = 3
+
+state = states.ROBOT_IDLE
+
+pubRobotSpeed = rospy.Publisher('robot_speed', UInt16, queue_size=10)
+
+def RangefinderFrontMB_CallBack(msg):
+    distance = msg.data
+    rospy.loginfo(rospy.get_caller_id() + "%i cm", distance)
+
+    if state == states.ROBOT_PATROL_FWD:
+        if distance > 200:
+            speed = 100
+        else:
+            state = states.ROBOT_APPROACH_FWD
+            
+    elif state == states.ROBOT_APPROACH_FWD:
+        if distance > 200:
+            state = states.ROBOT_PATROL_FWD
+            speed = 100
+        elif distance > 50:
+            speed = (distance - 25)
+        else:
+            state = states.ROBOT_DETERRENT_FWD
+            speed = 0
+
+    elif state == states.ROBOT_DETERRENT_FWD:
+        if distance > 200:
+            state = states.ROBOT_PATROL_FWD
+            speed = 100
+
+    pubRobotSpeed.publish(speed)
+    rospy.loginfo("Speed -> %i cm/s", speed)
+
+def main():
+    rospy.init_node('wire_bot')
+    rospy.Subscriber("/rangefinder/front/MB", UInt16, RangefinderFrontMB_CallBack)
+
+    while not rospy.is_shutdown():
+        rospy.spinOnce()
+
+if __name__ == '__main__':
+    main()
+    

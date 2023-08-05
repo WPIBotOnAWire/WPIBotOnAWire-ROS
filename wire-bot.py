a459fd5a0fa6d9cd2ef798225e0685169b1a5c76
Greg <lewin.gc@gmail.com>
2023-08-05 15:00:53 -0400

    state in the right spot

--- wire-bot.py
+++ wire-bot.py
@@ -9,12 +9,9 @@ from std_msgs.msg import UInt16
 
 class states(Enum):
     ROBOT_IDLE = 0, 
-
     ROBOT_PATROL_FWD = 1
     ROBOT_APPROACH_FWD = 2
     ROBOT_DETERRENT_FWD = 3
-
-state = states.ROBOT_IDLE
 
 pubRobotSpeed = rospy.Publisher('robot_speed', UInt16, queue_size=10)
 
@@ -49,6 +46,8 @@ def RangefinderFrontMB_CallBack(msg):
 def main():
     rospy.init_node('wire_bot')
     rospy.Subscriber("/rangefinder/front/MB", UInt16, RangefinderFrontMB_CallBack)
+
+    state = states.ROBOT_IDLE
 
     while not rospy.is_shutdown():
         rospy.spinOnce()

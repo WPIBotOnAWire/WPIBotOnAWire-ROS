3a50387940dfd8f5ee84a7db9ff03d40be49eccd
Puen <xupuen1223@gmail.com>
2023-10-24 14:40:37 -0400

    Able to detect birds from live camera feed

--- bird_detection/src/liveCam.py
+++ bird_detection/src/liveCam.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
+
 # https://dipankarmedh1.medium.com/real-time-object-detection-with-yolo-and-webcam-enhancing-your-computer-vision-skills-861b97c78993
+
+import rospy
+from std_msgs.msg import Bool
 
 from ultralytics import YOLO
 import cv2
@@ -7,6 +12,9 @@ import math
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
+
+# Publishers
+pubBirdExistence = rospy.Publisher('/bird_detect', Bool, queue_size=10)
 
 # model
 model = YOLO("yolo-Weights/yolov8n.pt")
@@ -24,47 +32,72 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "teddy bear", "hair drier", "toothbrush"
               ]
 
+def detect():
 
-while True:
-    success, img = cap.read()
-    results = model(img, stream=True)
+    while True:
 
-    # coordinates
-    for r in results:
-        boxes = r.boxes
+        success, img = cap.read()
+        results = model(img, stream=True)
 
-        for box in boxes:
-            # bounding box
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+        findBird = False
 
-            # put box in cam
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        # coordinates
+        for r in results:
 
-            # confidence
-            confidence = math.ceil((box.conf[0]*100))/100
-            #print("Confidence --->",confidence)
+            boxes = r.boxes
 
-            # class name
-            cls = int(box.cls[0])
-            #print("Class name -->", classNames[cls])
+            for box in boxes:
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+                # put box in cam
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+                # confidence
+                confidence = math.ceil((box.conf[0]*100))/100
+                #print("Confidence --->",confidence)
+
+                # class name
+                cls = int(box.cls[0])
+                #print("Class name -->", classNames[cls])
+
+                # object details
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+
+                if classNames[cls] == "bird" and confidence > 0.5: 
+
+                    findBird = True
+                    cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+        if findBird:
+
+            rospy.loginfo("Detected bird")
+            pubBirdExistence.publish(True)    
+
+        else: 
+
+            rospy.loginfo("No bird")
+            pubBirdExistence.publish(False)   
+
+        cv2.imshow('Webcam', img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
+def main():
+    rospy.init_node('liveCam')
 
-            # object details
-            org = [x1, y1]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            fontScale = 1
-            color = (255, 0, 0)
-            thickness = 2
+    detect()
 
-            if classNames[cls] == "bird" and confidence > 0.5: 
-                print("Found")
-                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+    rospy.spin()
 
-    cv2.imshow('Webcam', img)
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main()

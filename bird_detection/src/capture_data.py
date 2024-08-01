25da700269fc02e1f0550863a3daec3b70b219a6
Greg Lewin <gcl8a@icloud.com>
2024-08-01 17:50:09 -0400

    comments

--- bird_detection/src/capture_data.py
+++ bird_detection/src/capture_data.py
@@ -5,32 +5,40 @@ from std_msgs.msg import UInt16
 import os
 import cv2 as cv
 
-##TODO: Make sure that these are the right cam ports for the front and the back
+## TODO: Make sure that these are the right cam ports for the front and the back
+## For now, camera 1 commented out for testing
 frontCamera = cv.VideoCapture(0)
 #backCamera = cv.VideoCapture(1)
+
 #Used to give each image a unique name
-foreIndex = 0 #, aftIndex = 0
+foreIndex = 0 
+aftIndex = 0
+
 #Image path and naming schemes
 imagePath = "/home/boaw/bird_photos/"
 aftNameStem = "aft_img_"
 foreNameStem = "fore_img_"
+
 #Images will be saved as a png format, can easily be changed to a jpeg if needed
 imgType = ".png"
-lastImageTime = 0
+
+# Used to keep track of the last image so we don't save too many images
+lastImageTimeFore = 0
 
 def front_capture_callback(msg: UInt16):
     #If we are more than 2 meters away from a bird, don't bother taking a picture
-    global lastImageTime
+    global lastImageTimeFore
     global foreIndex
+
     currentTime = rospy.get_time()
     if msg.data < 200:
-        if currentTime - lastImageTime > 5:
+        if currentTime - lastImageTimeFore > 5:
             result, image = frontCamera.read()
             name = imagePath + foreNameStem + str(foreIndex) + "-bird" + imgType
             foreIndex += 1
             cv.imwrite(filename=name, img=image)
             rospy.loginfo(name)
-            lastImageTime = currentTime
+            lastImageTimeFore = currentTime
     
     else:
         if currentTime - lastImageTime > 20:

06260f2f95a23ea2d7018d0b18daa5c3b6977db4
Greg Lewin <gcl8a@icloud.com>
2024-08-01 18:21:24 -0400

    using class

--- bird_detection/src/capture_data.py
+++ bird_detection/src/capture_data.py
@@ -15,7 +15,7 @@ foreIndex = 0
 aftIndex = 0
 
 # #Image path and naming schemes
-# imagePath = "/home/boaw/bird_photos/"
+imagePath = "/home/boaw/bird_photos/"
 # #aftNameStem = "aft_img_"
 # #foreNameStem = "fore_img_"
 
@@ -29,11 +29,11 @@ class image_capture_manager:
     def __init__(self, topic, cam): 
         # initialize the subscriber node now. 
         self.distance_sub = rospy.Subscriber(topic, UInt16, self.distance_callback)
-        self.imagePath = "/home/boaw/bird_photos/"
         self.imgType = ".png"
         self.lastImageTime = 0
         self.file_index = 0
         self.camera_index = cam
+        self.camera = cv.VideoCapture(cam)
         
     def callback(self, distance_msg):
         distance = distance_msg.data
@@ -50,7 +50,7 @@ class image_capture_manager:
                 self.lastImageTime = currentTime
 
     def create_filename(self, hasBird):
-        name = self.imagePath + str(self.camera_index) + str(self.file_index) + str(hasBird) + self.imgType
+        name = imagePath + str(self.camera_index) + "-" + str(self.file_index) + str(hasBird) + self.imgType
         rospy.loginfo(name)
         self.file_index += 1
         return name
@@ -62,51 +62,39 @@ class image_capture_manager:
 
 
 
+# def front_capture_callback(msg: UInt16):
+#     global lastImageTimeFore
+#     global foreIndex
 
-
-def front_capture_callback(msg: UInt16):
-    global lastImageTimeFore
-    global foreIndex
-
-    currentTime = rospy.get_time()
-    if msg.data < 200:
-        if currentTime - lastImageTimeFore > 5:
-            result, image = frontCamera.read()
-            name = imagePath + foreNameStem + str(foreIndex) + "-bird" + imgType
-            foreIndex += 1
-            cv.imwrite(filename=name, img=image)
-            rospy.loginfo(name)
-            lastImageTimeFore = currentTime
+#     currentTime = rospy.get_time()
+#     if msg.data < 200:
+#         if currentTime - lastImageTimeFore > 5:
+#             result, image = frontCamera.read()
+#             name = imagePath + foreNameStem + str(foreIndex) + "-bird" + imgType
+#             foreIndex += 1
+#             cv.imwrite(filename=name, img=image)
+#             rospy.loginfo(name)
+#             lastImageTimeFore = currentTime
     
-    else:
-        if currentTime - lastImageTimeFore > 20:
-            result, image = frontCamera.read()
-            name = imagePath + foreNameStem + str(foreIndex) + "-nobird" + imgType
-            foreIndex += 1
-            cv.imwrite(filename = name, img = image)
-            rospy.loginfo(name)
-            lastImageTimeFore = currentTime
-    return
+#     else:
+#         if currentTime - lastImageTimeFore > 20:
+#             result, image = frontCamera.read()
+#             name = imagePath + foreNameStem + str(foreIndex) + "-nobird" + imgType
+#             foreIndex += 1
+#             cv.imwrite(filename = name, img = image)
+#             rospy.loginfo(name)
+#             lastImageTimeFore = currentTime
+#     return
 
-def back_capture_callback(msg: UInt16):
-
-    #If we are more than 2 meters away from a bird, don't bother taking a picture
-    while msg.data < 200:    
-        result, image = backCamera.read()
-        name = imagePath + aftNameStem + aftIndex + imgType
-        aftIndex += 1
-        cv.imwrite(filename=name, img=image)
-        #Sleep for a second so there aren't infinite photos of the cormorant
-        rospy.sleep(1)
-    return
 
 
 def main():
     if not os.path.exists(imagePath):
         os.mkdir(imagePath)
     rospy.init_node("capture_data")
-    lastImageTime = rospy.get_time()
-    rospy.Subscriber("/distance/fore", UInt16, front_capture_callback)
+
+    front_camera = image_capture_manager("/distance/fore", 0)
+#    rospy.Subscriber("/distance/fore", UInt16, front_capture_callback)
 #    rospy.Subscriber("/distance/aft", UInt16, back_capture_callback)
     rospy.spin()
 

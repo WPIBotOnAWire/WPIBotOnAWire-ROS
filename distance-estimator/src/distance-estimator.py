42b9c6ffa4c019b1651e6d6e7256f4175187e242
Puen <xupuen1223@gmail.com>
2024-08-01 14:49:54 -0400

    logic for taking pictures

--- distance-estimator/src/distance-estimator.py
+++ distance-estimator/src/distance-estimator.py
@@ -7,13 +7,13 @@ from std_msgs.msg import UInt16
 pubFrontDistance = rospy.Publisher('/distance/fore', UInt16, queue_size=10)
 pubAftDistance = rospy.Publisher('/distance/aft', UInt16, queue_size=10)
 
-newMBForeReading, newMBAftReading = False
-newTFForeReading, newTFAftReading = False
+# newMBForeReading, newMBAftReading = False
+# newTFForeReading, newTFAftReading = False
 
-frontMBDistance = 0
-aftMBDistance = 0
-frontTFDistance = 0
-aftTFDistance = 0
+# frontMBDistance = 0
+# aftMBDistance = 0
+# frontTFDistance = 0
+# aftTFDistance = 0
 
 # rosrun distance-estimator distance-estimator.py
 # tf faster than mb
@@ -37,14 +37,14 @@ def estimate_distance(distanceMB, distanceTF, isFore):
 
 def RangefinderFrontMB_CallBack(msg):
 
-    newMBForeReading = True
+#    newMBForeReading = True
     frontMBDistance = msg.data
         
     rospy.loginfo("Front(MB): %i cm", frontMBDistance)
 
-    if newTFForeReading:
-        estDist = estimate_distance(frontMBDistance, frontTFDistance, True)
-        pubFrontDistance.publish(estDist)
+#    if newTFForeReading:
+    estDist = frontMBDistance # estimate_distance(frontMBDistance, frontTFDistance, True)
+    pubFrontDistance.publish(estDist)
 
 
 
@@ -81,9 +81,9 @@ def main():
     rospy.init_node('distance_estimator')
 
     rospy.Subscriber("/rangefinder/fore/MB", UInt16, RangefinderFrontMB_CallBack)
-    rospy.Subscriber("/rangefinder/aft/MB", UInt16, RangefinderAftMB_CallBack)
-    rospy.Subscriber("/rangefinder/fore/------", UInt16, RangefinderFrontTF_CallBack)
-    rospy.Subscriber("/rangefinder/aft/TF", UInt16, RangefinderAftTF_CallBack)
+#    rospy.Subscriber("/rangefinder/aft/MB", UInt16, RangefinderAftMB_CallBack)
+#    rospy.Subscriber("/rangefinder/fore/------", UInt16, RangefinderFrontTF_CallBack)
+#    rospy.Subscriber("/rangefinder/aft/TF", UInt16, RangefinderAftTF_CallBack)
 
     rospy.spin()
 
